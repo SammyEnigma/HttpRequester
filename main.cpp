74e576ba48b648448ef799beda33182c42d11be8
Unknown <shahriar25.ss@gmail.com>
2018-02-08 18:56:24 +0330

    Added Qt project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQuickControls2/QQuickStyle>
+
+int main(int argc, char *argv[])
+{
+#if defined(Q_OS_WIN)
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+	QGuiApplication app(argc, argv);
+	QQuickStyle::setStyle("Material");
+
+	QQmlApplicationEngine engine;
+	engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+	if (engine.rootObjects().isEmpty()) return -1;
+
+	return app.exec();
+}
